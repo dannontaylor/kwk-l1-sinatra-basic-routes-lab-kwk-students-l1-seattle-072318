3fd911e8b9b8260c7cdf2c4629af5b851fc9401e
dannontaylor <taylordannon@gmail.com>
2018-07-31 20:41:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
+def say_hello(your_name)
+	puts “My name is #{your_name}!”
+end
+def say_hello(hometown)
+	puts “My hometown is #{hometown}!”
+end
+def say_hello(favorite-song)
+	puts “My favorite-song is #{favorite-song}!”
+end
+
+get '/' do
+  "Hello, World!"
+end
+
 class App < Sinatra::Base
 end
